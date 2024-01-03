55132dc3bb96ce99e59c4956d85c726f6ecc7e5c
Arsonide <arsonide@yahoo.com>
2024-01-03 15:58:28 -0600

    Improve the visual presentation of the medical database.

---
+++ Patches/MedDatabaseIconLabelPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace GovernmentLockdown;
+
+[HarmonyPatch(typeof(DesktopIconController), "Setup")]
+public class MedDatabaseIconLabelPatch
+{
+    [HarmonyPostfix]
+    private static void Postfix(DesktopIconController __instance, DesktopApp newDesktop, CruncherAppPreset newApp)
+    {
+        if (newApp.presetName != "GovDatabase")
+        {
+            return;
+        }
+        
+        switch (Utilities.GetMunicipalComputerType(newDesktop))
+        {
+            case MunicipalComputerType.HospitalSecure:
+            case MunicipalComputerType.HospitalInsecure:
+                __instance.iconText.text = "Med Database";
+                break;
+        }
+    }
+}
--- Patches/MedDatabasePrintsBirthCertificatesPatch.cs
+++ Patches/MedDatabasePrintsBirthCertificatesPatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using TMPro;
 using UnityEngine;
 
 namespace GovernmentLockdown;
@@ -6,12 +7,23 @@ namespace GovernmentLockdown;
 [HarmonyPatch(typeof(DatabaseApp), "OnSetup")]
 public class MedDatabasePrintsBirthCertificatesPatch
 {
+    private static InteractablePreset _printedCitizensReport = null;
     private static InteractablePreset _printedBirthCertificate = null;
-    
+
     [HarmonyPostfix]
     private static void Postfix(DatabaseApp __instance)
     {
-        // Set up the preset if it doesn't exist.
+        if (Utilities.GetMunicipalComputerType(__instance) == MunicipalComputerType.NotMunicipal)
+        {
+            return;
+        }
+        
+        // Set up the presets if they don't exist.
+        if (_printedCitizensReport == null)
+        {
+            _printedCitizensReport = __instance.ddsPrintout;
+        }
+        
         if (_printedBirthCertificate == null)
         {
             _printedBirthCertificate = Object.Instantiate(Toolbox.Instance.GetInteractablePreset("BirthCertificate"));
@@ -22,18 +34,33 @@ public class MedDatabasePrintsBirthCertificatesPatch
             
             // At this point, ddsPrintout will be the Citizen Report still from the government database.
             // Birth certificates are usually in files, and cannot be "taken", so we make them takeable by cloning the action list.
-            foreach (InteractableActionsPreset action in __instance.ddsPrintout.actionsPreset)
+            foreach (InteractableActionsPreset action in _printedCitizensReport.actionsPreset)
             {
                 _printedBirthCertificate.actionsPreset.Add(action);
             }
         }
 
+        ComputerAutoTextController autoText = __instance.titleText.gameObject.GetComponent<ComputerAutoTextController>();
+
+        if (autoText != null)
+        {
+            Object.DestroyImmediate(autoText);
+        }
+        
+        __instance.titleText.overflowMode = TextOverflowModes.Overflow;
+        __instance.titleText.enableWordWrapping = false;
+        
         // Make the swap if we're in a hospital.
         switch (Utilities.GetMunicipalComputerType(__instance))
         {
             case MunicipalComputerType.HospitalInsecure:
             case MunicipalComputerType.HospitalSecure:
+                __instance.titleText.text = "Medical Database";
                 __instance.ddsPrintout = _printedBirthCertificate;
+                break;
+            default:
+                __instance.titleText.text = "Citizens Database";
+                __instance.ddsPrintout = _printedCitizensReport;
                 break;
         }
     }
